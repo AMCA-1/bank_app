8eb67cd46cd32e97103bcdc4462654b6927bfa37
AMCA-1 <your_email@example.com>
2024-08-24 22:20:14 +0530

    removed footer

--- bank_app/src/components/CTA.jsx
+++ bank_app/src/components/CTA.jsx
@@ -13,6 +13,7 @@ const CTA = () => {
               Everything you need to accept card payments and grow your business anywhere on the planet.
             </p>
             <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-9`}> 
+              
 
             </div>
             <Button/>
